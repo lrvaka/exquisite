94b991e39653af2d9f0d72b46c2e2a6b5e44d38b
Luke Vakasisikakala <lukevakasisikakala@gmail.com>
2022-05-31 12:44:28 -0400

    gsap

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,33 @@
 import { ChakraProvider, Box, keyframes } from "@chakra-ui/react"
 import { theme } from "../lib/theme"
 import Fonts from "../lib/fonts"
+import { useRef, useEffect } from "react"
+import { gsap } from "gsap"
+import ScrollTrigger from "gsap/dist/ScrollTrigger"
+import ScrollSmoother from "gsap/dist/ScrollSmoother"
 
 if (typeof window !== "undefined") {
   window.history.scrollRestoration = "manual"
 }
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, data }) {
+  const SmoothScrollRef = useRef(null)
+  useEffect(() => {
+    gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
+
+    let smoother = ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      smooth: 2, // how long (in seconds) it takes to "catch up" to the native scroll position
+      effects: true, // looks for data-speed and data-lag attributes on elements
+    })
+  }, [SmoothScrollRef])
+
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
-      <Component {...pageProps} />
+      <Box id="smooth-wrapper" ref={SmoothScrollRef}>
+        <Component {...pageProps} />
+      </Box>
     </ChakraProvider>
   )
 }
