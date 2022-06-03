2730a8a3aea995559e798872be9c17d58de3c2e7
Luke Vakasisikakala <lukevakasisikakala@gmail.com>
2022-06-02 23:59:08 -0400

    mobile message

--- components/MessageSectionAnimation.desktop.js
+++ components/MessageSectionAnimation.desktop.js
@@ -1,11 +1,12 @@
 import { useState, useRef, useLayoutEffect, useEffect } from "react"
 import useIsomorphicLayoutEffect from "./hooks/useIsomorphicLayoutEffect"
 import ChakraBox from "./ChakraBox"
-import { Flex, Box, Heading, Grid } from "@chakra-ui/react"
+import { Flex, Box, Heading, Grid, useMediaQuery } from "@chakra-ui/react"
 import gsap from "gsap"
 import planks from "./planks"
 import NextImage from "next/image"
 import ScrollTrigger from "gsap/dist/ScrollTrigger"
+import ResponsiveComponent from "./ResponsiveComponent"
 
 const MessageSectionAnimationDesktop = ({ children, ...props }) => {
   const containerRef = useRef()
@@ -14,8 +15,6 @@ const MessageSectionAnimationDesktop = ({ children, ...props }) => {
     let left = gsap.utils.toArray(".left")
 
     let right = gsap.utils.toArray(".right")
-
-    let hide = (elem) => gsap.set(elem, { autoAlpha: 0 })
 
     right.forEach((plank, i) => {
       let tl = gsap.fromTo(
@@ -59,36 +58,35 @@ const MessageSectionAnimationDesktop = ({ children, ...props }) => {
       })
     })
 
-    return () => {}
-  }, [])
+    return () => {
+    
+    }
+  }, [containerRef])
 
   return (
-    <Box pos="relative" className="message" py="20" overflowX="hidden">
-      <Flex
-        pos="relative"
-        justifyContent="center"
-        flexDir="column"
-        maxH="100vh"
-        w="100vw"
-        alignItems="center"
-        ref={containerRef}
-      >
-        {planks.map((e, i) => (
-          <Flex flexDir="row" zIndex="1" key={i}>
-            {e.map((element, index) => (
-              <NextImage
-                key={index}
-                className={element.class}
-                src={element.src}
-                width={element.w}
-                height={element.h}
-                priority="true"
-              />
-            ))}
-          </Flex>
-        ))}
-      </Flex>
-    </Box>
+    <Flex
+      pos="relative"
+      justifyContent="center"
+      flexDir="column"
+      w="100vw"
+      alignItems="center"
+      ref={containerRef}
+    >
+      {planks.desktop.map((e, i) => (
+        <Flex flexDir="row" zIndex="1" key={i}>
+          {e.map((element, index) => (
+            <NextImage
+              key={index}
+              className={element.class}
+              src={element.src}
+              width={element.w}
+              height={element.h}
+              priority="true"
+            />
+          ))}
+        </Flex>
+      ))}
+    </Flex>
   )
 }
 
--- components/MessageSectionAnimation.js
+++ components/MessageSectionAnimation.js
@@ -1,9 +1,25 @@
-import { Box, useMediaQuery } from "@chakra-ui/react"
-import Desktop from "./MessageSectionAnimation.desktop"
+import { useState, useRef, useLayoutEffect, useEffect } from "react"
+import useIsomorphicLayoutEffect from "./hooks/useIsomorphicLayoutEffect"
+import ChakraBox from "./ChakraBox"
+import { Flex, Box, Heading, Grid, useMediaQuery } from "@chakra-ui/react"
+import gsap from "gsap"
+import planks from "./planks"
+import NextImage from "next/image"
+import ScrollTrigger from "gsap/dist/ScrollTrigger"
+import MessageSectionAnimationMobile from "./MessageSectionAnimation.mobile"
+import MessageSectionAnimationDesktop from "./MessageSectionAnimation.desktop"
+import ResponsiveComponent from "./ResponsiveComponent"
 
-const MessageSectionAnimation = () => {
-  //   const [isLargerThan1280] = useMediaQuery("(min-width: 1280px)")
-  return <>{<Desktop />}</>
+const MessageSectionAnimation = ({ children, ...props }) => {
+  return (
+    <Box pos="relative" className="message" py="20" overflowX="hidden">
+      <ResponsiveComponent
+        mobileSize="480"
+        mobileComponents={<MessageSectionAnimationMobile />}
+        desktopComponents={<MessageSectionAnimationDesktop />}
+      />
+    </Box>
+  )
 }
 
 export default MessageSectionAnimation
