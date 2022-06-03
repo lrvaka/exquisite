7edb374e4be604876fba635780d81c038512808a
Luke Vakasisikakala <lukevakasisikakala@gmail.com>
2022-06-02 20:17:23 -0400

    one sweep animation

--- components/planks.js
+++ components/planks.js
@@ -1,28 +1,53 @@
 const planks = [
-  { src: "/images/planks/1.png", w: "887", h: "92" },
-  { src: "/images/planks/2.png", w: "1033", h: "92" },
-  { src: "/images/planks/3.png", w: "1261", h: "92" },
-  { src: "/images/planks/4.png", w: "659", h: "92" },
-  { src: "/images/planks/5.png", w: "802", h: "91" },
-  { src: "/images/planks/6.png", w: "1118", h: "91" },
-  { src: "/images/planks/7.png", w: "1137", h: "92" },
-  { src: "/images/planks/8.png", w: "783", h: "92" },
-  { src: "/images/planks/9.png", w: "631", h: "92" },
-  { src: "/images/planks/10.png", w: "1289", h: "92" },
-  { src: "/images/planks/11.png", w: "887", h: "91" },
-  { src: "/images/planks/12.png", w: "1033", h: "91" },
-  { src: "/images/planks/13.png", w: "1261", h: "91" },
-  { src: "/images/planks/14.png", w: "659", h: "91" },
-  { src: "/images/planks/15.png", w: "740", h: "92" },
-  { src: "/images/planks/16.png", w: "1180", h: "92" },
-  { src: "/images/planks/17.png", w: "1046", h: "92" },
-  { src: "/images/planks/18.png", w: "874", h: "92" },
-  { src: "/images/planks/19.png", w: "863", h: "91" },
-  { src: "/images/planks/20.png", w: "1057", h: "92" },
-  { src: "/images/planks/21.png", w: "1111", h: "92" },
-  { src: "/images/planks/22.png", w: "809", h: "91" },
-  { src: "/images/planks/23.png", w: "802", h: "92" },
-  { src: "/images/planks/24.png", w: "1118", h: "92" },
+  [
+    { src: "/images/planks/1.png", w: "887", h: "92", class: "left" },
+    { src: "/images/planks/2.png", w: "1033", h: "92", class: "right" },
+  ],
+
+  [
+    { src: "/images/planks/3.png", w: "1261", h: "92", class: "left" },
+    { src: "/images/planks/4.png", w: "659", h: "92", class: "right" },
+  ],
+  [
+    { src: "/images/planks/5.png", w: "802", h: "91", class: "left" },
+    { src: "/images/planks/6.png", w: "1118", h: "91", class: "right" },
+  ],
+  [
+    { src: "/images/planks/7.png", w: "1137", h: "92", class: "left" },
+    { src: "/images/planks/8.png", w: "783", h: "92", class: "right" },
+  ],
+  [
+    { src: "/images/planks/9.png", w: "631", h: "92", class: "left" },
+    { src: "/images/planks/10.png", w: "1289", h: "92", class: "right" },
+  ],
+  [
+    { src: "/images/planks/11.png", w: "887", h: "91", class: "left" },
+    { src: "/images/planks/12.png", w: "1033", h: "91", class: "right" },
+  ],
+  [
+    { src: "/images/planks/13.png", w: "1261", h: "91", class: "left" },
+    { src: "/images/planks/14.png", w: "659", h: "91", class: "right" },
+  ],
+  [
+    { src: "/images/planks/15.png", w: "740", h: "92", class: "left" },
+    { src: "/images/planks/16.png", w: "1180", h: "92", class: "right" },
+  ],
+  [
+    { src: "/images/planks/17.png", w: "1046", h: "92", class: "left" },
+    { src: "/images/planks/18.png", w: "874", h: "92", class: "right" },
+  ],
+  [
+    { src: "/images/planks/19.png", w: "863", h: "91", class: "left" },
+    { src: "/images/planks/20.png", w: "1057", h: "92", class: "right" },
+  ],
+  [
+    { src: "/images/planks/21.png", w: "1111", h: "92", class: "left" },
+    { src: "/images/planks/22.png", w: "809", h: "91", class: "right" },
+  ],
+  [
+    { src: "/images/planks/23.png", w: "802", h: "92", class: "left" },
+    { src: "/images/planks/24.png", w: "1118", h: "92", class: "right" },
+  ],
 ]
 
 export default planks
