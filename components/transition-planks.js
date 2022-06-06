cd82100c30a186614c08ab2063bac4d61f2d1ce6
Luke Vakasisikakala <lukevakasisikakala@gmail.com>
2022-06-06 01:13:56 -0400

    wood transition

---
+++ components/transition-planks.js
@@ -0,0 +1,346 @@
+const transitionPlanks = {
+  desktop: [
+    [
+      {
+        src: "/images/transition/1.png",
+        w: "887",
+        h: "92",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/2.png",
+        w: "1033",
+        h: "92",
+        ref: "setRightPlankRefs",
+      },
+    ],
+
+    [
+      {
+        src: "/images/transition/3.png",
+        w: "1261",
+        h: "92",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/4.png",
+        w: "659",
+        h: "92",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/5.png",
+        w: "802",
+        h: "91",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/6.png",
+        w: "1118",
+        h: "91",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/7.png",
+        w: "1137",
+        h: "92",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/8.png",
+        w: "783",
+        h: "92",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/9.png",
+        w: "631",
+        h: "92",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/10.png",
+        w: "1289",
+        h: "92",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/11.png",
+        w: "887",
+        h: "91",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/12.png",
+        w: "1033",
+        h: "91",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/13.png",
+        w: "1261",
+        h: "91",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/14.png",
+        w: "659",
+        h: "91",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/15.png",
+        w: "740",
+        h: "92",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/16.png",
+        w: "1180",
+        h: "92",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/17.png",
+        w: "1046",
+        h: "92",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/18.png",
+        w: "874",
+        h: "92",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/19.png",
+        w: "863",
+        h: "91",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/20.png",
+        w: "1057",
+        h: "92",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/21.png",
+        w: "1111",
+        h: "92",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/22.png",
+        w: "809",
+        h: "91",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/23.png",
+        w: "802",
+        h: "92",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/24.png",
+        w: "1118",
+        h: "92",
+        ref: "setRightPlankRefs",
+      },
+    ],
+  ],
+  mobile: [
+    [
+      {
+        src: "/images/transition/mobile/1.png",
+        w: "499",
+        h: "164",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/mobile/2.png",
+        w: "581",
+        h: "164",
+        ref: "setRightPlankRefs",
+      },
+    ],
+
+    [
+      {
+        src: "/images/transition/mobile/3.png",
+        w: "709",
+        h: "162",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/mobile/4.png",
+        w: "371",
+        h: "162",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/mobile/5.png",
+        w: "451",
+        h: "162",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/mobile/6.png",
+        w: "629",
+        h: "162",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/mobile/7.png",
+        w: "640",
+        h: "164",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/mobile/8.png",
+        w: "440",
+        h: "164",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/mobile/9.png",
+        w: "355",
+        h: "164",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/mobile/10.png",
+        w: "725",
+        h: "164",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/mobile/11.png",
+        w: "499",
+        h: "162",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/mobile/12.png",
+        w: "581",
+        h: "162",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/mobile/13.png",
+        w: "709",
+        h: "162",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/mobile/14.png",
+        w: "371",
+        h: "162",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/mobile/15.png",
+        w: "416",
+        h: "164",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/mobile/16.png",
+        w: "664",
+        h: "164",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/mobile/17.png",
+        w: "588",
+        h: "164",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/mobile/18.png",
+        w: "492",
+        h: "163",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/mobile/19.png",
+        w: "486",
+        h: "162",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/mobile/20.png",
+        w: "594",
+        h: "164",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/mobile/21.png",
+        w: "625",
+        h: "162",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/mobile/22.png",
+        w: "455",
+        h: "161",
+        ref: "setRightPlankRefs",
+      },
+    ],
+    [
+      {
+        src: "/images/transition/mobile/23.png",
+        w: "451",
+        h: "164",
+        ref: "setLeftPlankRefs",
+      },
+      {
+        src: "/images/transition/mobile/24.png",
+        w: "629",
+        h: "164",
+        ref: "setRightPlankRefs",
+      },
+    ],
+  ],
+}
+
+export default transitionPlanks
--- pages/index.js
+++ pages/index.js
@@ -27,21 +27,6 @@ const MessageSection = dynamic(() =>
   import("../components/MessageSectionAnimation")
 )
 
-const grainAnimation = keyframes`
-  0%, 100% { transform:translate(0, 0); }
-  10% { transform:translate(-5%, -10%); }
-  20% { transform:translate(-15%, 5%); }
-  30% { transform:translate(7%, -25%); }
-  40% { transform:translate(-5%, 25%); }
-  50% { transform:translate(-15%, 10%); }
-  60% { transform:translate(15%, 0%); }
-  70% { transform:translate(0%, 15%); }
-  80% { transform:translate(3%, 35%); }
-  90% { transform:translate(-10%, 10%); }
-`
-
-const animation = `${grainAnimation} 8s steps(10) infinite`
-
 const MarqueeSection = () => (
   <Marquee gradient={false}>
     <Heading
