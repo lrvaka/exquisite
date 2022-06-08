01a75e3891a5f5bc166b23b014cbacdf410a5ebb
Luke Vakasisikakala <lukevakasisikakala@gmail.com>
2022-06-08 01:00:15 -0400

    from animation

--- components/ui/SectionHeading.js
+++ components/ui/SectionHeading.js
@@ -24,20 +24,16 @@ const SectionHeading = ({ children, ...props }) => {
     //   skewX: 10,
     // })
 
-    let splitCharsAni = gsap.fromTo(
-      split.chars,
-      { autoAlpha: 0.1, y: 10, scale: 1.25, skewX: 10 },
-      {
-        y: 0,
-        opacity: 1,
-        scale: 1,
-        skewX: 0,
-        duration: 1,
-        stagger: 0.01,
-        ease: "a1",
-        scrollTrigger: ref.current,
-      }
-    )
+    let splitCharsAni = gsap.from(split.chars, {
+      autoAlpha: 0.1,
+      y: 10,
+      scale: 1.25,
+      skewX: 10,
+      duration: 1,
+      stagger: 0.01,
+      ease: "a1",
+      scrollTrigger: ref.current,
+    })
   }, [])
 
   return (
