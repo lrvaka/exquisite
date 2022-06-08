f0d963ecb4f2d1637ee1d939947b10d3c30343ec
Luke Vakasisikakala <lukevakasisikakala@gmail.com>
2022-06-08 05:07:39 -0400

    font preload

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="preload"
+          href=href="https://use.typekit.net/qoq0ljs.css"
+          as="font"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
