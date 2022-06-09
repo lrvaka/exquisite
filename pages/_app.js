516b76a8be51e36ba111166d499847742b712d05
Luke Vakasisikakala <lukevakasisikakala@gmail.com>
2022-06-09 05:14:52 -0400

    effects disabled

--- pages/_app.js
+++ pages/_app.js
@@ -45,7 +45,6 @@ function MyApp({ Component, pageProps }) {
       wrapper: wrapperRef.current,
       content: contentRef.current,
       smooth: 2, // how long (in seconds) it takes to "catch up" to the native scroll position
-      effects: true, // looks for data-speed and data-lag attributes on elements
     })
 
     setTimeout(() => {
