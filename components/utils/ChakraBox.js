16dd01c689e1caf2c18deb7e029c62351491b9d1
Luke Vakasisikakala <lukevakasisikakala@gmail.com>
2022-06-10 17:06:40 -0400

    view more works

---
+++ components/utils/ChakraBox.js
@@ -0,0 +1,8 @@
+import { chakra } from "@chakra-ui/react"
+import { motion, isValidMotionProp } from "framer-motion"
+
+const ChakraBox = chakra(motion.div, {
+  shouldForwardProp: isValidMotionProp,
+})
+
+export default ChakraBox
