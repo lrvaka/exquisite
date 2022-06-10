de28fe31647de3ba5ed9ae2cba3cfeb8c2ff60ad
Luke Vakasisikakala <lukevakasisikakala@gmail.com>
2022-06-10 17:06:40 -0400

    view more works

--- pages/works.js
+++ pages/works.js
@@ -10,6 +10,7 @@ import {
   Text,
   Grid,
 } from "@chakra-ui/react"
+import WorksModal from "../components/ui/Modal"
 import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 import workSlides from "../lib/work-slides"
@@ -20,6 +21,7 @@ import MainWrapper from "../components/ui/Main"
 import Footer from "../components/ui/Footer"
 import clients from "../lib/clients"
 import gsap from "gsap"
+import ChakraBox from "../components/utils/ChakraBox"
 import GridItem from "../components/WorksComponents/GridItem"
 import GridImage from "../components/WorksComponents/GridImage"
 import ParallaxGridItem from "../components/WorksComponents/ParallaxGridItem"
@@ -32,11 +34,44 @@ import gridImage5 from "../public/images/works/orchard/3.jpg"
 import gridImage6 from "../public/images/works/orchard/2.jpg"
 import gridImage7 from "../public/images/works/orchard/7.jpg"
 import gridImage8 from "../public/images/works/orchard/6.jpg"
-import gridImage9 from "../public/images/works/orchard/4.jpg"
-import gridImage10 from "../public/images/works/orchard/1.jpg"
+import gridImage9 from "../public/images/works/orchard/9.jpg"
+import gridImage10 from "../public/images/works/hearst/1.jpg"
+import gridImage11 from "../public/images/works/hearst/2.jpg"
+import gridImage12 from "../public/images/works/hearst/3.jpg"
+import gridImage13 from "../public/images/works/hearst/4.jpg"
+import gridImage14 from "../public/images/works/hearst/5.jpg"
+import gridImage15 from "../public/images/works/gucci/1.jpg"
+import gridImage16 from "../public/images/works/gucci/2.jpg"
+import gridImage17 from "../public/images/works/gucci/3.jpg"
+import gridImage18 from "../public/images/works/gucci/4.jpg"
+import gridImage20 from "../public/images/works/gucci/6.jpg"
+import gridImage21 from "../public/images/works/gucci/7.jpg"
 import useArrayRef from "../components/hooks/useArrayRef"
+import Marquee from "react-fast-marquee"
 
-const animation = { duration: 50000, easing: (t) => t }
+const MarqueeText = ({ children, ...props }) => (
+  <Heading
+    fontWeight="black"
+    color="brand.400"
+    fontSize={["2rem", "3rem", "4rem", "5rem", "6rem"]}
+    opacity="0.25"
+    overflowY="hidden"
+    {...props}
+  >
+    {children}
+  </Heading>
+)
+
+const MarqueeSection = ({ ...props }) => (
+  <Marquee gradient={false}>
+    <Flex gap="24" {...props}>
+      <MarqueeText>Reliable</MarqueeText>
+      <MarqueeText>Quality</MarqueeText>
+      <MarqueeText>Craftsmanship</MarqueeText>
+      <MarqueeText mr="24">Pristine</MarqueeText>
+    </Flex>
+  </Marquee>
+)
 
 const WorksHeading = () => (
   <Container pos="relative" maxW="container.xl" px="4" minH="70vh">
@@ -62,7 +97,7 @@ const WorksHeading = () => (
           lineHeight="normal"
           pb="9"
         >
-          Here is a few of our selected works from over the years. From
+          Below is a display of selected works from over the years. From
           commercial to residential, installation to restoration, we&apos;ve had
           the pleasure of working on various projects from a wide array of
           clientele.
@@ -87,43 +122,6 @@ const Works = () => {
   const q = gsap.utils.selector(el)
   const [gridItems, setGridItems] = useArrayRef()
   const ifAnimate = useRef(false)
-
-  const [sliderRef] = useKeenSlider({
-    loop: true,
-    renderMode: "performance",
-    mode: "free",
-    created(s) {
-      s.moveToIdx(10, true, animation)
-    },
-    updated(s) {
-      s.moveToIdx(s.track.details.abs + 10, true, animation)
-    },
-    animationEnded(s) {
-      s.moveToIdx(s.track.details.abs + 10, true, animation)
-    },
-    slides: {
-      perView: 3,
-      spacing: 15,
-    },
-  })
-  const [sliderRef1] = useKeenSlider({
-    loop: true,
-    renderMode: "performance",
-    mode: "free",
-    created(s) {
-      s.moveToIdx(-10, true, animation)
-    },
-    updated(s) {
-      s.moveToIdx(s.track.details.abs - 10, true, animation)
-    },
-    animationEnded(s) {
-      s.moveToIdx(s.track.details.abs - 10, true, animation)
-    },
-    slides: {
-      perView: 3,
-      spacing: 15,
-    },
-  })
 
   useIsomorphicLayoutEffect(() => {
     if (!gridItems.current) {
@@ -159,7 +157,7 @@ const Works = () => {
         minH={["500px", "800px"]}
         templateColumns="repeat(10, 1fr)"
         templateRows="repeat(10,1fr)"
-        mb="60"
+        mb="40"
         ref={el}
       >
         <Heading
@@ -172,8 +170,10 @@ const Works = () => {
           color="white"
           fontWeight="black"
           fontSize={["6xl", "9xl"]}
+          lineHeight="100%"
         >
-          Reliable
+          Carolina <br />
+          Herrera
         </Heading>
         <GridItem
           ref={setGridItems}
@@ -207,6 +207,8 @@ const Works = () => {
         />
       </Grid>
 
+      <MarqueeSection />
+
       <Grid
         pos="relative"
         maxW="container.xl"
@@ -214,7 +216,7 @@ const Works = () => {
         minH={["500px", "800px"]}
         templateColumns="repeat(10, 1fr)"
         templateRows="repeat(10,1fr)"
-        mb="60"
+        my="40"
       >
         <Heading
           zIndex="2"
@@ -225,9 +227,11 @@ const Works = () => {
           left="50%"
           color="white"
           fontWeight="black"
+          lineHeight="100%"
           fontSize={["6xl", "9xl"]}
         >
-          Artisanal
+          196 <br />
+          Orchard
         </Heading>
         <GridItem
           ref={setGridItems}
@@ -249,7 +253,7 @@ const Works = () => {
           ref={setGridItems}
           gridRow="1 / 5"
           gridColumn="8 / 11"
-          src={gridImage10}
+          src={gridImage9}
         />
         <GridItem
           ref={setGridItems}
@@ -266,6 +270,9 @@ const Works = () => {
           src={gridImage7}
         />
       </Grid>
+
+      <MarqueeSection />
+
       <Grid
         pos="relative"
         maxW="container.xl"
@@ -273,7 +280,7 @@ const Works = () => {
         minH={["500px", "800px"]}
         templateColumns="repeat(10, 1fr)"
         templateRows="repeat(10,1fr)"
-        mb="60"
+        my="40"
         ref={el}
       >
         <Heading
@@ -285,90 +292,146 @@ const Works = () => {
           left="50%"
           color="white"
           fontWeight="black"
+          lineHeight="100%"
           fontSize={["6xl", "9xl"]}
         >
-          Exquisite
+          Gabriela <br /> Hearst
         </Heading>
         <GridItem
           ref={setGridItems}
           data-speed="1.1"
           gridRow="1 / 10"
           gridColumn="1 / 6"
-          src={gridImage1}
+          src={gridImage10}
         />
 
         <GridItem
           ref={setGridItems}
-          data-speed="1.2"
           gridRow="1 / 11"
           gridColumn="6 / 11"
-          src={gridImage3}
+          src={gridImage11}
         />
 
         <GridItem
           ref={setGridItems}
           data-speed="1.3"
-          gridRow="5 / 10"
-          gridColumn="3 / 8"
-          src={gridImage4}
+          gridRow="1 / 4"
+          gridColumn="8 / 11"
+          src={gridImage12}
         />
 
         <GridItem
           ref={setGridItems}
-          gridRow="6 / 10"
-          gridColumn="1 / 5"
-          src={gridImage2}
+          gridRow="8 / 11"
+          gridColumn="2 / 4"
+          src={gridImage13}
+        />
+        <GridItem
+          ref={setGridItems}
+          data-speed="1.2"
+          gridRow="1 / 4"
+          gridColumn="3 / 7"
+          src={gridImage14}
         />
       </Grid>
-      {/* <Box
-        py="24"
-        overflow="hidden"
-        pos="relative"
-        ref={sliderRef}
-        className="keen-slider"
-      >
-        {workSlides.carolina.map((e, i) => (
-          <Box className="keen-slider__slide" key={i} h="15rem">
-            <GridImage src={e} />
-          </Box>
-        ))}
-      </Box>
+
+      <MarqueeSection />
+
       <Grid
+        pos="relative"
         maxW="container.xl"
         m="0 auto"
         minH={["500px", "800px"]}
         templateColumns="repeat(10, 1fr)"
         templateRows="repeat(10,1fr)"
+        my="40"
+        ref={el}
       >
-        <ParallaxGridItem gridRow="6 / 11" gridColumn="1/ 3" src={gridImage5} />
+        <Heading
+          zIndex="2"
+          mixBlendMode="difference"
+          pos="absolute"
+          transform="translate(-50%,-50%)"
+          top="50%"
+          left="50%"
+          color="white"
+          fontWeight="black"
+          lineHeight="100%"
+          fontSize={["6xl", "9xl"]}
+        >
+          Gucci
+        </Heading>
 
-        <GridItem gridRow="1 / 6" gridColumn="1 / 5" src={gridImage6} />
+        <GridItem
+          ref={setGridItems}
+          data-speed="1.3"
+          gridRow="1 / 7"
+          gridColumn="6 / 11"
+          src={gridImage17}
+        />
 
-        <ParallaxGridItem gridRow="1 / 6" gridColumn="5 / 8" src={gridImage7} />
+        <GridItem
+          ref={setGridItems}
+          data-speed="0.9"
+          gridRow="4 / 11"
+          gridColumn="2 / 7"
+          src={gridImage18}
+        />
 
-        <GridItem gridRow="1 / 7" gridColumn="8/ 11" src={gridImage8} />
-
-        <GridItem gridRow="6 / 11" gridColumn="3 / 8" src={gridImage9} />
-
-        <ParallaxGridItem
-          gridRow="7 / 11"
-          gridColumn="8/ 11"
-          src={gridImage10}
+        <GridItem
+          ref={setGridItems}
+          gridRow="5 / 11"
+          gridColumn="8 / 11"
+          src={gridImage20}
+        />
+        <GridItem
+          ref={setGridItems}
+          gridRow="7 / 10"
+          gridColumn="1 / 3"
+          src={gridImage21}
+        />
+        <GridItem
+          ref={setGridItems}
+          data-speed="1.2"
+          gridRow="9 / 11"
+          gridColumn="5 / 8"
+          src={gridImage16}
+        />
+        <GridItem
+          ref={setGridItems}
+          data-speed="1.1"
+          gridRow="2 / 5"
+          gridColumn="1 / 5"
+          src={gridImage15}
         />
       </Grid>
-      <Box
-        overflow="hidden"
+
+      <Flex
+        justifyContent="center"
+        alignItems="center"
         pos="relative"
-        ref={sliderRef1}
-        py="24"
-        className="keen-slider"
+        maxW="container.xl"
+        m="0 auto"
+        mb="40"
+        minH="150px"
+        ref={el}
       >
-        {workSlides.grafbro.map((e, i) => (
-          <Box className="keen-slider__slide" key={i} h="15rem">
-            <GridImage src={e} />
-          </Box>
-        ))}
-      </Box> */}
+        <WorksModal>
+          <Heading
+            zIndex="2"
+            textAlign="center"
+            gridRow=" 1 / 5"
+            gridColumn="4 / 8"
+            color="brand.400"
+            fontWeight="100"
+            lineHeight="100%"
+            fontSize={["4xl", "5xl"]}
+          >
+            View More
+          </Heading>
+        </WorksModal>
+      </Flex>
+
       <ContactSection
         bgColor="brand.500"
         contactFormVariant="work"
