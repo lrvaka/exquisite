2900641e3225f6e6c2bd8675dcc15c20553e2cf6
Luke Vakasisikakala <lukevakasisikakala@gmail.com>
2022-06-24 12:29:41 -0400

    Google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,22 @@
 import { Html, Head, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 export default function Document() {
   return (
     <Html>
       <Head>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}')
+        `}
+        </Script>
         <link rel="stylesheet" href="https://use.typekit.net/qoq0ljs.css" />
       </Head>
       <body>
