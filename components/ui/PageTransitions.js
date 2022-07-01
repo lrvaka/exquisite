711f6d6f03ea0ad53b9b10685ebbae46b6a30d62
lrvaka <lukevakasisikakala@gmail.com>
2022-07-01 11:56:46 -0400

    add mobile & large screen page transitions

--- components/ui/PageTransitions.js
+++ components/ui/PageTransitions.js
@@ -1,182 +1,41 @@
-import styled from "@emotion/styled"
-import { keyframes } from "@emotion/react"
+import ResponsiveComponent from "../utils/ResponsiveComponent"
+import PageTransitionsOther from "./PageTransitions.other"
+import PageTransitionsDesktop from "./PageTransitions.desktop"
+import PageTransitionsMobile from "./PageTransitions.mobile"
+import { useContext } from "react"
 import GsapContext from "../../store/gsap-context"
-import { Flex } from "@chakra-ui/react"
-import { TransitionGroup, CSSTransition } from "react-transition-group"
-import { useState, useRef, useEffect, useContext } from "react"
-import gsap from "gsap"
-import transitionPlanks from "../../lib/transition-planks"
-import Navbar from "./Navbar"
-import { useRouter } from "next/router"
-import NextImage from "next/image"
-import useArrayRef from "../hooks/useArrayRef"
-import useIsomorphicLayoutEffect from "../hooks/useIsomorphicLayoutEffect"
-
-const MainComponent = styled.div`
-  position: "relative";
-
-  &.page-enter-active {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    z-index: 4;
-    opacity: 0;
-  }
-
-  &.page-exit-active {
-    main {
-      transform: translateY(-${(props) => props.routingPageOffset}px);
-    }
-  }
-`
-
-const Grid = styled.div`
-  flex-direction: column;
-  justify-content: center;
-  display: flex;
-  pointer-events: none;
-  z-index: 5;
-  width: 100%;
-  height: 100vh;
-  top: 0;
-  left: 0;
-  position: fixed;
-  visibility: hidden;
-  flex: 1;
-`
 
 const PageTransitions = ({ children, route, routingPageOffset }) => {
-  const { contentRef } = useContext(GsapContext)
-  const tl = useRef()
-  const tl1 = useRef()
-  const transitionRef = useRef()
-  const [leftPlankRefs, setLeftPlankRefs] = useArrayRef()
-  const [rightPlankRefs, setRightPlankRefs] = useArrayRef()
-
-  let plankType
-
-  const playTransition = () => {
-    tl.current.play(0)
-    tl1.current.play(0)
-  }
-  const stopTransition = () => {}
-
-  useEffect(() => {
-    if (!transitionRef.current) {
-      return
-    }
-
-    gsap.set(transitionRef.current, { autoAlpha: 1 })
-
-    tl.current = gsap
-      .timeline({
-        repeat: 1,
-        repeatDelay: 0.5,
-        yoyo: true,
-        paused: true,
-      })
-      .fromTo(
-        leftPlankRefs.current,
-        { x: window.innerWidth * -1, autoAlpha: 0 },
-        {
-          x: 0,
-          autoAlpha: 1,
-          ease: "power4.out",
-          stagger: {
-            ease: "sine",
-            amount: 0.5,
-            from: "random",
-          },
-        }
-      )
-
-    tl1.current = gsap
-      .timeline({
-        repeat: 1,
-        repeatDelay: 0.5,
-        yoyo: true,
-        paused: true,
-      })
-      .fromTo(
-        rightPlankRefs.current,
-        { x: window.innerWidth, autoAlpha: 0  },
-        {
-          x: 0,
-          autoAlpha: 1,
-          ease: "power4.out",
-          stagger: {
-            ease: "sine",
-            amount: 0.5,
-            from: "random",
-          },
-        }
-      )
-
-    return () => {
-      tl.current.kill()
-      tl1.current.kill()
-    }
-  }, [])
-
   return (
     <>
-      <TransitionGroup component={null}>
-        <CSSTransition
-          key={route}
-          timeout={1000}
-          classNames="page"
-          onEnter={playTransition}
-          onExited={stopTransition}
-        >
-          <MainComponent ref={contentRef} routingPageOffset={routingPageOffset}>
-            {children}
-          </MainComponent>
-        </CSSTransition>
-      </TransitionGroup>
-      <Grid ref={transitionRef}>
-        {transitionPlanks.desktop.map((e, i) => (
-          <Flex
-            alignItems="stretch"
-            pos="relative"
-            flexDir="row"
-            justifyContent="center"
-            zIndex="1"
-            key={i}
-            flex="1"
+      <ResponsiveComponent
+        mobileSize="428"
+        mobileComponents={
+          <PageTransitionsMobile
+            route={route}
+            routingPageOffset={routingPageOffset}
           >
-            {e.map((element, index) => {
-              if (element.ref === "setRightPlankRefs") {
-                plankType = setRightPlankRefs
-              }
-              if (element.ref === "setLeftPlankRefs") {
-                plankType = setLeftPlankRefs
-              }
-              return (
-                <Flex
-                  pos="relative"
-                  height="auto"
-                  key={index}
-                  ref={plankType}
-                  alignItems="stretch"
-                  bgColor="#705d56"
-                  border="1px solid #a1938e"
-                  visibility="hidden"
-                >
-                  <Flex minW={element.w} minH={element.h} />
-                  {/* <NextImage
-                    placeholder="blur"
-                    loading="lazy"
-                    src={element.component}
-                    // width={element.w}
-                    // height={element.h}
-                  /> */}
-                </Flex>
-              )
-            })}
-          </Flex>
-        ))}
-      </Grid>
+            {children}
+          </PageTransitionsMobile>
+        }
+        otherSize="1920"
+        otherComponents={
+          <PageTransitionsOther
+            route={route}
+            routingPageOffset={routingPageOffset}
+          >
+            {children}
+          </PageTransitionsOther>
+        }
+        desktopComponents={
+          <PageTransitionsDesktop
+            route={route}
+            routingPageOffset={routingPageOffset}
+          >
+            {children}
+          </PageTransitionsDesktop>
+        }
+      />
     </>
   )
 }
