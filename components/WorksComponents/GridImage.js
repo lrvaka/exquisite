55cfb53daf3adb8b64719b163433e6ffde2eca44
lrvaka <lukevakasisikakala@gmail.com>
2022-07-13 08:23:25 -0400

    change sizes of parallaximage, gridimage, etc to 25vw

--- components/WorksComponents/GridImage.js
+++ components/WorksComponents/GridImage.js
@@ -1,7 +1,15 @@
 import NextImage from "next/image"
 
 const GridImage = ({ alt, ...props }) => (
-  <NextImage alt={alt} priority="true" placeholder="blur" layout="fill" objectFit="cover" {...props} />
+  <NextImage
+    alt={alt}
+    sizes="25vw"
+    priority="true"
+    placeholder="blur"
+    layout="fill"
+    objectFit="cover"
+    {...props}
+  />
 )
 
 export default GridImage
