328dd74d44db34b692d49f2e0cebb70ebb2f5442
lrvaka <lukevakasisikakala@gmail.com>
2022-08-29 12:38:01 -0400

    switch to www

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://exquisitewoodfloors.com",
+  siteUrl: process.env.SITE_URL || "https://www.exquisitewoodfloors.com/",
   generateRobotsTxt: true, // (optional)
   // ...other options
-}
+};
