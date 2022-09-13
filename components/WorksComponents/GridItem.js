3f3a85898ca0eceed6e4e1a016224157b868965a
lrvaka <lukevakasisikakala@gmail.com>
2022-09-13 19:31:30 -0400

    44 studios

--- components/WorksComponents/GridItem.js
+++ components/WorksComponents/GridItem.js
@@ -1,37 +1,12 @@
 import GridImage from "./GridImage";
-import { useRef } from "react";
+import { forwardRef } from "react";
 import { Box } from "@chakra-ui/react";
-import useIsomorphicLayoutEffect from "../hooks/useIsomorphicLayoutEffect";
-
-import gsap from "gsap";
 
 // eslint-disable-next-line react/display-name
-const GridItem = ({ src, alt, ...props }) => {
-  const ref = useRef();
-
-  useIsomorphicLayoutEffect(() => {
-    gsap.fromTo(
-      ref.current,
-      {
-        opacity: 0.1,
-        scale: 0.75,
-        clipPath: "inset(100% 0 0 0)",
-      },
-      {
-        opacity: 1,
-        scale: 1,
-        clipPath: "inset(0% 0% 0% 0%)",
-        duration: 2,
-        scrollTrigger: ref.current,
-      }
-    );
-  }, []);
-
-  return (
-    <Box pos="relative" overflow="hidden" {...props} ref={ref}>
-      <GridImage alt={alt} src={src} />
-    </Box>
-  );
-};
+const GridItem = forwardRef(({ src, alt, ...props }, ref) => (
+  <Box pos="relative" overflow="hidden" {...props} ref={ref}>
+    <GridImage alt={alt} src={src} />
+  </Box>
+));
 
 export default GridItem;
