b28f6b3668182f051199f472d0aacf9118c7a917
lrvaka <lukevakasisikakala@gmail.com>
2022-09-13 19:39:09 -0400

    google shortcut

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document"
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/qoq0ljs.css" />
+        <link rel="icon" href="/favicon.ico" type="image/png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
