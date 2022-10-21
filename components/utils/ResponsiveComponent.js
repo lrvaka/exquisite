03127b6ddc2d940c6dbf0c5807970f04ae56fba5
lrvaka <lukevakasisikakala@gmail.com>
2022-10-21 19:51:35 -0400

    fix ssr window object

--- components/utils/ResponsiveComponent.js
+++ components/utils/ResponsiveComponent.js
@@ -6,14 +6,19 @@ function useMediaQuery(query) {
   const [matches, setMatches] = useState(false);
 
   useEffect(() => {
-    const matchQueryList = window.matchMedia(query);
-    function handleChange(e) {
-      setMatches(e.matches);
+    if (typeof window !== "undefined") {
+      const matchQueryList = window.matchMedia(query);
+
+      function handleChange(e) {
+        setMatches(e.matches);
+      }
+
+      matchQueryList.addEventListener("change", handleChange);
+
+      return () => {
+        matchQueryList.removeEventListener("change", handleChange);
+      };
     }
-    matchQueryList.addEventListener("change", handleChange);
-    return () => {
-      matchQueryList.removeEventListener("change", handleChange);
-    };
   }, [query]);
 
   return matches;
