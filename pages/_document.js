f19567d69f7224dd167dbd42fc35e30a923e915b
lrvaka <lukevakasisikakala@gmail.com>
2022-11-01 00:09:02 -0400

    add google ads

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,6 +8,20 @@ export default function Document() {
         <link rel="stylesheet" href="https://use.typekit.net/qoq0ljs.css" />
         <link rel="icon" href="/favicon.ico" type="image/png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ADS}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ADS}')
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
