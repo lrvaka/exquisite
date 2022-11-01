98e1e31602b285e4c9e9eef7a46ad1eb9c57529f
lrvaka <lukevakasisikakala@gmail.com>
2022-10-31 23:52:42 -0400

    fix thankyou page

--- components/ui/Navbar/Navbar.js
+++ components/ui/Navbar/Navbar.js
@@ -3,9 +3,11 @@ import NextLink from "next/link";
 import GsapContext from "../../../store/gsap-context";
 import { useContext } from "react";
 import NavbarItems from "./NavbarItems";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
   const { smoother } = useContext(GsapContext);
+  const router = useRouter();
 
   return (
     <header className="w-full fixed z-20 md:bg-[#dbe2bb]/50">
@@ -24,29 +26,31 @@ const Navbar = () => {
               />
             </a>
           </NextLink>
-          <nav className="flex self-center items-center flex-row-reverse md:flex-row gap-2 md:gap-8">
-            <NavbarItems />
-            <button
-              className="flex p-2 text-theme-10 border-[1px] border-theme-500 border-solid font-heading bg-theme-400 items-center max-w-max text-lg 2xl:text-2xl font-bold"
-              onClick={() =>
-                smoother.scrollTo("#contact", true, "center center")
-              }
-            >
-              Contact
-              <svg
-                className="ml-2"
-                stroke="currentColor"
-                fill="currentColor"
-                strokeWidth="0"
-                viewBox="0 0 1024 1024"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
+          {router.asPath === "/thank-you" ? null : (
+            <nav className="flex self-center items-center flex-row-reverse md:flex-row gap-2 md:gap-8">
+              <NavbarItems />
+              <button
+                className="flex p-2 text-theme-10 border-[1px] border-theme-500 border-solid font-heading bg-theme-400 items-center max-w-max text-lg 2xl:text-2xl font-bold"
+                onClick={() =>
+                  smoother.scrollTo("#contact", true, "center center")
+                }
               >
-                <path d="M928 160H96c-17.7 0-32 14.3-32 32v640c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V192c0-17.7-14.3-32-32-32zm-40 110.8V792H136V270.8l-27.6-21.5 39.3-50.5 42.8 33.3h643.1l42.8-33.3 39.3 50.5-27.7 21.5zM833.6 232L512 482 190.4 232l-42.8-33.3-39.3 50.5 27.6 21.5 341.6 265.6a55.99 55.99 0 0 0 68.7 0L888 270.8l27.6-21.5-39.3-50.5-42.7 33.2z"></path>
-              </svg>
-            </button>
-          </nav>
+                Contact
+                <svg
+                  className="ml-2"
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 1024 1024"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M928 160H96c-17.7 0-32 14.3-32 32v640c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V192c0-17.7-14.3-32-32-32zm-40 110.8V792H136V270.8l-27.6-21.5 39.3-50.5 42.8 33.3h643.1l42.8-33.3 39.3 50.5-27.7 21.5zM833.6 232L512 482 190.4 232l-42.8-33.3-39.3 50.5 27.6 21.5 341.6 265.6a55.99 55.99 0 0 0 68.7 0L888 270.8l27.6-21.5-39.3-50.5-42.7 33.2z"></path>
+                </svg>
+              </button>
+            </nav>
+          )}
         </div>
       </div>
     </header>
