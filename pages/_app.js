0f9dbcf200f019d724a46a613e47d68803a3ad0f
lrvaka <lukevakasisikakala@gmail.com>
2022-11-07 11:52:57 -0500

    fix tag

--- pages/_app.js
+++ pages/_app.js
@@ -75,20 +75,6 @@ function MyApp({ Component, pageProps }) {
         `}
       </Script>
 
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ADS}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ADS}')
-        `}
-      </Script>
-
       <GsapContext.Provider
         value={{
           smoother,
