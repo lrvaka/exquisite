cf17fb85368ee09f95c0df5a12ec9eb2b54c9c83
lrvaka <lukevakasisikakala@gmail.com>
2022-11-07 10:52:51 -0500

    fix meta for thank you

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import Head from "next/head";
 
 export default function ThankYou() {
   const router = useRouter();
@@ -9,15 +10,22 @@ export default function ThankYou() {
       router.push("/");
     }, 3000);
   }, []);
-  
+
   return (
-    <div className="h-screen flex justify-center items-center flex-col">
-      <h3 className="font-heading font-black text-6xl text-theme-500">
-        Thank you!
-      </h3>
-      <p className="text-2xl md:text-3xl text-theme-500 text-center">
-        Thank you for your submission, we will get back to you shortly!
-      </p>
-    </div>
+    <>
+      <Head>
+        <meta name="robots" content="noindex" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <meta property="og:title" content="Thank You - Exquisite Wood Floors" />
+      </Head>
+      <div className="h-screen flex justify-center items-center flex-col">
+        <h3 className="font-heading font-black text-6xl text-theme-500">
+          Thank you!
+        </h3>
+        <p className="text-2xl md:text-3xl text-theme-500 text-center">
+          Thank you for your submission, we will get back to you shortly!
+        </p>
+      </div>
+    </>
   );
 }
