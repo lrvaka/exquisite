f5057be2a18339a0786cbbfeccbd5a3ef525b586
lrvaka <lukevakasisikakala@gmail.com>
2022-11-09 19:05:29 -0500

    gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import { gsap } from "gsap";
+import { GTM_ID, pageview } from "../lib/gtm";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import ScrollSmoother from "gsap/dist/ScrollSmoother";
 import GsapContext from "../store/gsap-context";
@@ -52,6 +53,11 @@ function MyApp({ Component, pageProps }) {
     };
 
     router.events.on("routeChangeStart", pageChange);
+    router.events.on("routeChangeComplete", pageview);
+    return () => {
+      router.events.off("routeChangeComplete", pageview);
+      router.events.off("routeChangeStart", pageChange);
+    };
   }, [router.events]);
 
   useIsomorphicLayoutEffect(() => {
@@ -74,6 +80,20 @@ function MyApp({ Component, pageProps }) {
         gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}')
         `}
       </Script>
+
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
 
       <GsapContext.Provider
         value={{
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
+import { GTM_ID } from "../lib/gtm";
 
 export default function Document() {
   return (
@@ -8,9 +8,16 @@ export default function Document() {
         <link rel="stylesheet" href="https://use.typekit.net/qoq0ljs.css" />
         <link rel="icon" href="/favicon.ico" type="image/png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Main />
         <NextScript />
       </body>
