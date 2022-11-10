5c092315e969f0f945f2d65f3e99b1bd3a00d12c
lrvaka <lukevakasisikakala@gmail.com>
2022-11-10 08:54:30 -0500

    fb pixel

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import { FB_PIXEL_ID } from "../lib/fpixel";
 
 export default function Document() {
   return (
@@ -8,9 +8,16 @@ export default function Document() {
         <link rel="stylesheet" href="https://use.typekit.net/qoq0ljs.css" />
         <link rel="icon" href="/favicon.ico" type="image/png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
       </Head>
       <body>
-   
         <Main />
         <NextScript />
       </body>
