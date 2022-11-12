64cd47ba9626c2cea6fab87d61e94561f5cbcb5d
lrvaka <lukevakasisikakala@gmail.com>
2022-11-11 21:31:54 -0500

    mobile header

--- components/ui/Navbar/Navbar.js
+++ components/ui/Navbar/Navbar.js
@@ -63,33 +63,42 @@ const Navbar = () => {
   );
 
   const MobileNav = () => (
-    <div className="mx-auto relative max-w-[2300px]  md:bg-[#dbe2bb]/50">
-      <div className="flex justify-between p-4">
-        <NextLink href="/" passHref>
-          <a
-            aria-label="home button"
-            className="max-w-[90px] 2xl:max-w-[125px]"
-          >
-            <NextImage
-              alt="ewf logo"
-              src="/images/dark-logo.png"
-              width={312}
-              height={348}
-            />
-          </a>
-        </NextLink>
-        {router.asPath === "/thank-you" ? null : (
-          <nav className="flex self-center items-center flex-row-reverse md:flex-row gap-2 md:gap-8">
-            <NavbarItems />
-            <NextLink href="/contact" passHref>
-              <a className="flex p-2 text-theme-10 border-[1px] border-theme-500 border-solid font-heading bg-theme-400 items-center max-w-max text-lg 2xl:text-2xl font-bold">
-                Get In Touch
-              </a>
-            </NextLink>
-          </nav>
-        )}
+    <Headroom
+      style={{
+        webkitTransition: "all 1s ease-in-out",
+        mozTransition: "all 1s ease-in-out",
+        oTransition: "all 1s ease-in-out",
+        transition: "all 1s ease-in-out",
+      }}
+    >
+      <div className="mx-auto relative max-w-[2300px]  bg-[#dbe2bb]/50">
+        <div className="flex justify-between p-4">
+          <NextLink href="/" passHref>
+            <a
+              aria-label="home button"
+              className="max-w-[90px] 2xl:max-w-[125px]"
+            >
+              <NextImage
+                alt="ewf logo"
+                src="/images/dark-logo.png"
+                width={312}
+                height={348}
+              />
+            </a>
+          </NextLink>
+          {router.asPath === "/thank-you" ? null : (
+            <nav className="flex self-center items-center flex-row-reverse md:flex-row gap-2 md:gap-8">
+              <NavbarItems />
+              <NextLink href="/contact" passHref>
+                <a className="flex p-2 text-theme-10 border-[1px] border-theme-500 border-solid font-heading bg-theme-400 items-center max-w-max text-lg 2xl:text-2xl font-bold">
+                  Get In Touch
+                </a>
+              </NextLink>
+            </nav>
+          )}
+        </div>
       </div>
-    </div>
+    </Headroom>
   );
 
   return (
